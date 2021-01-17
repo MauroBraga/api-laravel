b10c23508317e7e73ef6b6c88b1c215666fb974f
MauroBraga <mrb0305@gmail.com>
2021-01-17 16:41:23 -0300

    commit api funcionando

--- api-laravel/app/Http/Controllers/ArticleController.php
+++ api-laravel/app/Http/Controllers/ArticleController.php
@@ -4,38 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Article;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    public function index()
+
+    public static function index()
     {
         return Article::all();
     }
 
-    public function show(Article $article)
+    public function show($id)
     {
-        return $article;
+        return Article::find($id);
     }
 
     public function store(Request $request)
     {
-        $article = Article::create($request->all());
-
-        return response()->json($article, 201);
+        return Article::create($request->all());
     }
 
-    public function update(Request $request, Article $article)
+    public function update(Request $request, $id)
     {
+        $article = Article::findOrFail($id);
         $article->update($request->all());
 
-        return response()->json($article, 200);
+        return $article;
     }
 
-    public function delete(Article $article)
+    public function delete(Request $request, $id)
     {
+        $article = Article::findOrFail($id);
         $article->delete();
 
-        return response()->json(null, 204);
+        return 204;
     }
+
 }
