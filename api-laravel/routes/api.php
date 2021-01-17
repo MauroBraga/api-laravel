01772608010be4bd1cfea22bf14ac0c8b8473770
MauroBraga <mrb0305@gmail.com>
2021-01-17 15:56:40 -0300

    commit api sem autenticação

---
+++ api-laravel/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+Use App\Article;
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
