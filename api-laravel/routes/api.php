cb4f6a970716c64e4f6d64edcd9a355158569c76
MauroBraga <mrb0305@gmail.com>
2021-01-17 21:26:28 -0300

    Login  retornando token

--- api-laravel/routes/api.php
+++ api-laravel/routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Article;
 use App\Http\Controllers\ArticleController;
 
+Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
+Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
+
 Route::get('articles', 'App\Http\Controllers\ArticleController@index');
 Route::get('articles/{id}', 'App\Http\Controllers\ArticleController@show');
 Route::post('articles', 'App\Http\Controllers\ArticleController@store');
