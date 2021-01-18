414b70a805ddd2a25703505e99d656e4e419b226
MauroBraga <mrb0305@gmail.com>
2021-01-17 21:37:32 -0300

    Login e logou funcionando

--- api-laravel/routes/api.php
+++ api-laravel/routes/api.php
@@ -6,10 +6,11 @@ use App\Models\Article;
 use App\Http\Controllers\ArticleController;
 
 Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
+Route::post('logout', 'App\Http\Controllers\Auth\LoginController@logout');
 Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
 
-Route::get('articles', 'App\Http\Controllers\ArticleController@index');
-Route::get('articles/{id}', 'App\Http\Controllers\ArticleController@show');
-Route::post('articles', 'App\Http\Controllers\ArticleController@store');
-Route::put('articles/{id}', 'App\Http\Controllers\ArticleController@update');
-Route::delete('articles/{id}', 'App\Http\Controllers\ArticleController@delete');
+Route::middleware('auth:api')->get('articles', 'App\Http\Controllers\ArticleController@index');
+Route::middleware('auth:api')->get('articles/{id}', 'App\Http\Controllers\ArticleController@show');
+Route::middleware('auth:api')->post('articles', 'App\Http\Controllers\ArticleController@store');
+Route::middleware('auth:api')->put('articles/{id}', 'App\Http\Controllers\ArticleController@update');
+Route::middleware('auth:api')->delete('articles/{id}', 'App\Http\Controllers\ArticleController@delete');
